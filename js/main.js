aa77e65520ba82ad30e67f84c1aeea89a618e31c
TK992 <kttp415@gmail.com>
2023-06-03 11:31:10 +0900

    fourteenth commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
 'use strict'
 
 {
-  // load後にローディングアイコンを削除する
+  // ---------- ローディング周りの動き ----------
+  // load後にローディングアイコンを削除
   const loader = document.querySelector('#loader')
   window.addEventListener('load', () => {
     loader.style.display = 'none';
   });
-
-  // load後に各コンテンツを表示させる
+  
+  // load後に各コンテンツを表示
   window.addEventListener('load', function() {
     const loaded = document.querySelectorAll('.load')
-      loaded.forEach(load => {
-        load.classList.remove('load')
-      })
-    });
+    loaded.forEach(load => {
+      load.classList.remove('load')
+    })
+  });
+  
 
-
-
-  // IntersectionObserverで要素を20%読み込んだらクラスを付与する
+  
+  
+  // ---------- .appear要素が20％読み込まれたら表示する ----------
   function appearItems() {
     const targets = document.querySelectorAll('.appear');
     
+    // showをすると同時に監視を止める
     function callback(entries, obs) {
       entries.forEach(entry => {
         if(!entry.isIntersecting) {
@@ -31,7 +34,8 @@
         }
       }
     )};
-    
+      
+    // 20%の読み込みを監視
     const options = {
       threshold: 0.2,
     };
@@ -42,23 +46,25 @@
     });
   }
   window.addEventListener('load', appearItems);
+    
 
 
-
-  // ナビゲーションのボタンをクリックしたらメニューを表示・非表示する
+    
+  // ---------- メニューボタンクリックでナビを表示・非表示 ----------
   const btn = document.querySelector('.header__button');
   const menu = document.querySelector('.header__nav-sp');
 
-  // SP用ナビゲーション表示
+  // SP用ナビを表示
   btn.addEventListener('click', ()=> {
     menu.style.display = 'flex';
   
+    // transitionを効かせるためにsetTimeout使用
     setTimeout(() => {
       menu.classList.add('open');
     }, 10);
   });
 
-  // SP用ナビゲーション消す
+  // リンク先をクリックしたらSP用ナビを非表示
   const menuLink = document.querySelectorAll('.header__nav-sp--link');
   menuLink.forEach(elm => {
       elm.addEventListener('click', ()=> {
@@ -66,21 +72,27 @@
         
         setTimeout(() => {
           menu.style.display = 'none'
-        }, 300);
+        }, 310);
     });
   });
 
-  // メニューのカルーセル
+
+
+
+  // ---------- Swiperの設定 ----------
+  // fadeでじんわりスライド
   const swiper = new Swiper('.swiper', {
     effect: 'fade',
     fadeEffect: {
       crossFade: true,
     },
 
+    // paginationをつける
     pagination: {
       el: '.swiper-pagination',
     },
   
+    // 「次へ」「戻る」ボタンをつける
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -89,39 +101,38 @@
 
   
 
-  // 「photo」セクションのスライドアニメーション
-  // 横1列に並べたスライドを2分割し、一方に追従させてループを持続する
+  // ---------- スライドショーのアニメーション ----------
+  const slidesOver = document.querySelector('.photo__wrapper--over');
+  const slideOver = Array.from(slidesOver.children);
+  const slideWidth = slideOver[0].width;
 
-  // elm：アニメーションさせるclass
-  // start：スライド開始位置
-  // end：スライド終了位置
-  // iteration：アニメーション開始時の進行度
-  function slideAnimation(elm, start, end, iteration) {
-    const slides = document.querySelectorAll(elm);
+  // 画像を複製して最終行に追加（2周目終了後に1枚目の画像に戻すため）
+  slideOver.forEach((slide) => {
+    const cloneSlide = slide.cloneNode(true);
+    cloneSlide.classList.add('clone');
+    slidesOver.appendChild(cloneSlide);
+  });
 
-    slides.forEach((slide) => {
-      slide.animate( [ {
-        transform : `translateX(${start})`,
-        } , {
-        transform : `translateX(${end})` ,
-      } ] , {
-        iterationStart: `${iteration}`,
-        duration: 30000,
-        iterations: Infinity
-      });
-    });
-  };
+  // 複製分を加えた画像の合計
+  const sumSlidesOver = Array.from(slidesOver.children);
+  // スライドショーの横幅
+  slidesOver.style.width = `${(slideWidth + 16) * (sumSlidesOver.length)}px`;
 
-  // 要素が読み込まれたらアニメーション開始
-  window.addEventListener("DOMContentLoaded", () => {
-    // 上段のスライドアニメーション
-    slideAnimation('.photo__slide--over', '600%', '-600%', 0);
-    slideAnimation('.photo__slide--over2', '0%', '-1200%',0.5);
-    // 下段のスライドアニメーション
-    slideAnimation('.photo__slide--under', '-600%', '600%', 0);
-    slideAnimation('.photo__slide--under2', '-1200%', '0%',0.5);
-  })
+  // スライドショーを動かす関数
+  function loopSlides() {
+    slidesOver.style.transition = 'none';
+    slidesOver.style.transform = 'translateX(16px)';
+    setTimeout(() => {
+      slidesOver.style.transition = 'transform 30s linear';
+      slidesOver.style.transform = `translateX(${-slideWidth * sumSlidesOver.length - 16}px)`;
+    }, 10);
+  }
 
+  // スライドショーの実行
+  loopSlides();
+  let intervalId = setInterval(loopSlides, 30010);
+
+  
 
   
   // photoセクションの画像クリックでモーダルを表示
